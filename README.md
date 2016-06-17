a7e5c8f2ed307de1862e4a8991fe1752194a1030
Artur Szott <artur.szott@holidaycheck.com>
2016-06-17 12:49:08 +0200

    Adjust module metadata and allow some plugin configuration

--- README.md
+++ README.md
@@ -1,17 +1,11 @@
-npm module boilerplate
+Webpack quotes module
 ===
 
-Boilerplate for creating a npm/browserify module using tape.
+Reached already the maximum speed of webpack? Try psychology.
 
-[![Build Status](https://travis-ci.org/krambuhl/npm-module-boilerplate.svg)](https://travis-ci.org/krambuhl/npm-module-boilerplate)
+Reduce the feeling of slow compilation by providing great meaningful quote to think about.
+
+[![Build Status](https://travis-ci.org/Arturszott/webpack-patience.svg?branch=master)](https://travis-ci.org/Arturszott/webpack-patience)
 
 Getting Started
 ---
-
-```bash
-$ git clone git@github.com:krambuhl/npm-module-boilerplate.git module
-$ cd module
-$ rm -rf .git
-$ git init
-$ git remote add origin https://github.com/user/repo.git
-```
--- index.js
+++ index.js
@@ -1,3 +1,50 @@
-module.exports = function() {
+'use strict';
 
+var chalk = require('chalk'),
+    notifier = require('node-notifier'),
+    defaultQuotes = require('./data/quotes.json');
+
+function pickRandomQuote(quotes) {
+    return quotes[Math.floor(Math.random() * quotes.length)];
+}
+
+function WebpackQuotes(config) {
+    this.additionalQuotes = config.additionalQuotes || [];
+    this.secondsBeforeApology = config.secondsBeforeApology || 20;
+}
+
+WebpackQuotes.prototype.apply = function (compiler) {
+    var welcomeMessage = '- I\'m compiling the assets, have you ever thought about the following words?\n',
+        additionalQuotes = this.additionalQuotes,
+        secondsBeforeApology = this.secondsBeforeApology,
+        allQuotes = defaultQuotes.patience.concat(additionalQuotes);
+
+    compiler.plugin('compile', function () {
+        var quote = pickRandomQuote(allQuotes);
+
+        console.log(chalk.bold.white('\n\nWebpack: ') + chalk.white(welcomeMessage));
+        console.log(chalk.gray('   ' + quote.text));
+        console.log(chalk.gray('  ― ' + chalk.underline(quote.author) + '\n\n'));
+    });
+
+    compiler.plugin('this-compilation', function (compilation) {
+        var start = Date.now();
+
+        compilation.plugin('optimize-assets', function (assets, callback) {
+            var secondsElapsed = (Date.now() - start) / 1000,
+                apology = secondsElapsed > secondsBeforeApology ? ' Sorry for the delay.' : '',
+                webpackQuote = '- Sir, your assets are ready!' + apology;
+
+            notifier.notify({
+                title: 'Webpack says:',
+                message: webpackQuote
+            });
+
+            console.log(chalk.bold.white('\n\nWebpack: ') + chalk.white(webpackQuote) + '\n\n');
+
+            callback();
+        });
+    });
 };
+
+module.exports = WebpackQuotes;
